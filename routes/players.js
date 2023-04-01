3c6f5daedf9eddc8c332bd4be980ae11721d5ba8
unknown <marco.luci95@gmail.com>
2023-04-01 16:46:08 -0400

    fixes siakam description

--- routes/players.js
+++ routes/players.js
@@ -2,7 +2,16 @@ const fs = require("fs");
 const express = require("express");
 const router = express.Router();
 
-//(Hopefully) gets all players
+// I have these two router.get's that I want to connect to frontend server
+// With the single player.get, can I fill my card properties with this? I believe
+// the params should allow for me to do this
+
+//Do I even need the get all players get request?
+//I think I may need this for when I want to populate the drop down list of all the player names
+//If I want to split the dropdown list so that one has Raptors players and the other has Celtics players --
+//Should I be using two separate js files? How can I do this?
+
+//Gets all players
 router.get("/", function (request, response) {
   const playersFile = fs.readFileSync("./data/players.json");
   const playersArray = JSON.parse(playersFile);
@@ -26,7 +35,7 @@ router.get("/", function (request, response) {
   response.json(mappedPlayers);
 });
 
-// Gets a single player - Not working
+// Gets a single player
 router.get("/:id", function (request, response) {
   const playersFile = fs.readFileSync("./data/players.json");
   const playersArray = JSON.parse(playersFile);
@@ -42,3 +51,7 @@ router.get("/:id", function (request, response) {
 });
 
 module.exports = router;
+
+// get requests look like:
+// http://localhost2323/players
+// http://localhost2323/players/fredvanvleet
